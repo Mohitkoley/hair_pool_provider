5385c98557026c637b3f24c8262fcaa506fea2b3
Mohit Koley <mohitkoley5@gmail.com>
2025-01-26 17:02:55 +0530

    Update Flutter application path in generated configuration files

--- lib/feature/auth/view/sign_in_screen.dart
+++ lib/feature/auth/view/sign_in_screen.dart
@@ -1,5 +1,5 @@
-import 'package:get/get.dart';
 import 'package:demandium_provider/utils/core_export.dart';
+import 'package:get/get.dart';
 
 class SignInScreen extends StatefulWidget {
   final bool exitFromApp;
@@ -16,7 +16,7 @@ class SignInScreenState extends State<SignInScreen> {
   final TextEditingController _passwordController = TextEditingController();
   bool _canExit = GetPlatform.isWeb ? true : false;
   final GlobalKey<FormState> signInFormKey = GlobalKey<FormState>();
-  
+
   String? emailErrorText;
   String? passwordErrorText;
 
@@ -33,7 +33,8 @@ class SignInScreenState extends State<SignInScreen> {
         if (_canExit) {
           exit(0);
         } else {
-          showCustomSnackBar('back_press_again_to_exit'.tr,  type: ToasterMessageType.info);
+          showCustomSnackBar('back_press_again_to_exit'.tr,
+              type: ToasterMessageType.info);
           _canExit = true;
           Timer(const Duration(seconds: 2), () {
             _canExit = false;
@@ -41,57 +42,68 @@ class SignInScreenState extends State<SignInScreen> {
         }
       },
       child: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        body: SafeArea(child: Center(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Center(
           child: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
             padding: const EdgeInsets.all(Dimensions.paddingSizeLarge),
             child: Center(
               child: GetBuilder<AuthController>(builder: (authController) {
-
                 return Form(
                   key: signInFormKey,
                   autovalidateMode: AutovalidateMode.onUserInteraction,
                   child: Column(children: [
-                    Image.asset(Images.logo,width: Dimensions.logoWidth,),
+                    Image.asset(
+                      Images.logo,
+                      width: Dimensions.logoWidth,
+                    ),
                     const SizedBox(height: Dimensions.paddingSizeExtraLarge),
-
                     const SizedBox(height: Dimensions.paddingSizeLarge),
                     CustomTextField(
-                      onCountryChanged: (countryCode) => authController.countryDialCode = countryCode.dialCode!,
-                      countryDialCode: authController.isNumberLogin  ? authController.countryDialCode : null,
+                      onCountryChanged: (countryCode) => authController
+                          .countryDialCode = countryCode.dialCode!,
+                      countryDialCode: authController.isNumberLogin
+                          ? authController.countryDialCode
+                          : null,
                       title: 'email_or_phone'.tr,
                       hintText: 'enter_email_or_password'.tr,
                       controller: _emailController,
                       focusNode: _emailFocus,
                       nextFocus: _passwordFocus,
                       inputType: TextInputType.emailAddress,
-                      onChanged: (String text){
+                      onChanged: (String text) {
                         final numberRegExp = RegExp(r'^[+]?[0-9]+$');
 
-                        if(text.isEmpty && authController.isNumberLogin){
+                        if (text.isEmpty && authController.isNumberLogin) {
                           authController.toggleIsNumberLogin();
                         }
-                        if(text.startsWith(numberRegExp) && !authController.isNumberLogin ){
+                        if (text.startsWith(numberRegExp) &&
+                            !authController.isNumberLogin) {
                           authController.toggleIsNumberLogin();
                           _emailController.text = text.replaceAll("+", "");
                         }
                         final emailRegExp = RegExp(r'@');
-                        if(text.contains(emailRegExp) && authController.isNumberLogin){
+                        if (text.contains(emailRegExp) &&
+                            authController.isNumberLogin) {
                           authController.toggleIsNumberLogin();
                         }
-
                       },
-                      onValidate: (String? value){
-
-                        if(authController.isNumberLogin && ValidationHelper.getValidPhone(authController.countryDialCode+value!) == ""){
-                        return "enter_valid_phone_number".tr;
+                      onValidate: (String? value) {
+                        if (authController.isNumberLogin &&
+                            ValidationHelper.getValidPhone(
+                                    authController.countryDialCode + value!) ==
+                                "") {
+                          return "enter_valid_phone_number".tr;
                         }
-                        return (GetUtils.isPhoneNumber(value!.tr) || GetUtils.isEmail(value.tr)) ? null : 'enter_email_address_or_phone_number'.tr;
+                        return (GetUtils.isPhoneNumber(value!.tr) ||
+                                GetUtils.isEmail(value.tr))
+                            ? null
+                            : 'enter_email_address_or_phone_number'.tr;
                       },
                     ),
-
-                    const SizedBox(height: Dimensions.paddingSizeExtraMoreLarge),
+                    const SizedBox(
+                        height: Dimensions.paddingSizeExtraMoreLarge),
                     CustomTextField(
                       title: 'password'.tr,
                       hintText: '********'.tr,
@@ -100,14 +112,13 @@ class SignInScreenState extends State<SignInScreen> {
                       inputType: TextInputType.visiblePassword,
                       isPassword: true,
                       inputAction: TextInputAction.done,
-                      onValidate: (String? value){
+                      onValidate: (String? value) {
                         return FormValidationHelper().isValidPassword(value);
                       },
                     ),
-
-
                     const SizedBox(height: Dimensions.paddingSizeSmall),
-                    Row(mainAxisAlignment: MainAxisAlignment.end,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Expanded(
                           child: ListTile(
@@ -120,13 +131,16 @@ class SignInScreenState extends State<SignInScreen> {
                                     activeColor: Theme.of(context).primaryColor,
                                     value: authController.isActiveRememberMe,
                                     checkColor: Colors.white,
-                                    onChanged: (bool? isChecked) => authController.toggleRememberMe(),
+                                    onChanged: (bool? isChecked) =>
+                                        authController.toggleRememberMe(),
                                   ),
                                 ),
-                                const SizedBox(width: Dimensions.paddingSizeSmall),
+                                const SizedBox(
+                                    width: Dimensions.paddingSizeSmall),
                                 Text(
                                   'remember_me'.tr,
-                                  style: robotoRegular.copyWith(fontSize: Dimensions.fontSizeDefault),
+                                  style: robotoRegular.copyWith(
+                                      fontSize: Dimensions.fontSizeDefault),
                                 ),
                               ],
                             ),
@@ -136,47 +150,77 @@ class SignInScreenState extends State<SignInScreen> {
                           ),
                         ),
                         TextButton(
-                          style: TextButton.styleFrom(minimumSize: const Size(1, 40),backgroundColor: Theme.of(context).colorScheme.surface),
-                          onPressed: () => Get.toNamed(RouteHelper.getSendOtpScreen()),
-                          child: Text('forgot_password?'.tr, style: robotoRegular.copyWith(
-                            fontSize: Dimensions.fontSizeDefault, color: Theme.of(context).colorScheme.tertiary.withOpacity(0.8),)
-                          ),
+                          style: TextButton.styleFrom(
+                              minimumSize: const Size(1, 40),
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.surface),
+                          onPressed: () =>
+                              Get.toNamed(RouteHelper.getSendOtpScreen()),
+                          child: Text('forgot_password?'.tr,
+                              style: robotoRegular.copyWith(
+                                fontSize: Dimensions.fontSizeDefault,
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .tertiary
+                                    .withOpacity(0.8),
+                              )),
                         ),
                       ],
                     ),
-
                     const SizedBox(height: Dimensions.paddingSizeLarge),
-
-                    CustomButton(btnTxt: 'sign_in'.tr, onPressed:  () => _login(authController) , isLoading: authController.isLoading!,),
-
-                    Get.find<SplashController>().configModel.content?.providerSelfRegistration == 1 ?
-                    Column(
-                      children: [
-                        const SizedBox(height: Dimensions.paddingSizeDefault),
-
-                        Text("or".tr, style: robotoRegular.copyWith(fontSize: Dimensions.fontSizeSmall,
-                              color: Theme.of(context).textTheme.bodyLarge!.color,)),
-                        Row(mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('do_not_have_an_account'.tr, style: robotoRegular.copyWith(fontSize: Dimensions.fontSizeDefault,
-                                    color: Theme.of(context).primaryColor)),
-                            TextButton(
-                              style: TextButton.styleFrom(minimumSize: const Size(1, 40),
-                                  backgroundColor: Theme.of(context).colorScheme.surface),
-                              onPressed: () => Get.toNamed(RouteHelper.signUp),
-
-                              child:Text('register_here'.tr,
-                                style: robotoRegular.copyWith(
-                                    color: Theme.of(context).colorScheme.tertiary.withOpacity(.8),
-                                    fontSize: Dimensions.fontSizeDefault
-                                ),
+                    CustomButton(
+                      btnTxt: 'sign_in'.tr,
+                      onPressed: () => _login(authController),
+                      isLoading: authController.isLoading!,
+                    ),
+                    Get.find<SplashController>()
+                                .configModel
+                                .content
+                                ?.providerSelfRegistration ==
+                            1
+                        ? Column(
+                            children: [
+                              const SizedBox(
+                                  height: Dimensions.paddingSizeDefault),
+                              Text("or".tr,
+                                  style: robotoRegular.copyWith(
+                                    fontSize: Dimensions.fontSizeSmall,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge!
+                                        .color,
+                                  )),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('do_not_have_an_account'.tr,
+                                      style: robotoRegular.copyWith(
+                                          fontSize: Dimensions.fontSizeDefault,
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  TextButton(
+                                    style: TextButton.styleFrom(
+                                        minimumSize: const Size(1, 40),
+                                        backgroundColor: Theme.of(context)
+                                            .colorScheme
+                                            .surface),
+                                    onPressed: () =>
+                                        Get.toNamed(RouteHelper.signUp),
+                                    child: Text(
+                                      'register_here'.tr,
+                                      style: robotoRegular.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .tertiary
+                                              .withOpacity(.8),
+                                          fontSize: Dimensions.fontSizeDefault),
+                                    ),
+                                  )
+                                ],
                               ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ) :const SizedBox.shrink(),
-
+                            ],
+                          )
+                        : const SizedBox.shrink(),
                   ]),
                 );
               }),
@@ -187,33 +231,40 @@ class SignInScreenState extends State<SignInScreen> {
     );
   }
 
+  _initializeController() {
+    var authController = Get.find<AuthController>();
+    String phoneWithoutCountryCode = ValidationHelper.getValidPhone(
+        Get.find<AuthController>().getUserNumber());
+    String countryCode = ValidationHelper.getCountryCode(
+        Get.find<AuthController>().getUserNumber());
 
-  _initializeController(){
-    var authController  = Get.find<AuthController>();
-    String phoneWithoutCountryCode =  ValidationHelper.getValidPhone(Get.find<AuthController>().getUserNumber());
-    String countryCode = ValidationHelper.getCountryCode(Get.find<AuthController>().getUserNumber());
-
-    WidgetsBinding.instance.addPostFrameCallback((_){
-      if(countryCode != "" && phoneWithoutCountryCode !=""){
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (countryCode != "" && phoneWithoutCountryCode != "") {
         authController.toggleIsNumberLogin(value: true);
-      }else{
+      } else {
         authController.toggleIsNumberLogin(value: false);
       }
-      authController.initCountryCode(countryCode: countryCode !="" ? countryCode : null);
-
+      authController.initCountryCode(
+          countryCode: countryCode != "" ? countryCode : null);
     });
 
-    _emailController.text = phoneWithoutCountryCode != "" ? phoneWithoutCountryCode : authController.isNumberLogin ? "" : Get.find<AuthController>().getUserNumber();
+    _emailController.text = phoneWithoutCountryCode != ""
+        ? phoneWithoutCountryCode
+        : authController.isNumberLogin
+            ? ""
+            : Get.find<AuthController>().getUserNumber();
     _passwordController.text = Get.find<AuthController>().getUserPassword();
   }
 
   void _login(AuthController authController) async {
-    if(signInFormKey.currentState!.validate()){
-      String phone = ValidationHelper.getValidPhone(authController.countryDialCode + _emailController.text.trim(), withCountryCode: true);
-      await authController.login( phone !="" ? phone : _emailController.text.trim(), _passwordController.text.trim(), phone !="" ? "phone" : "email");
+    if (signInFormKey.currentState!.validate()) {
+      String phone = ValidationHelper.getValidPhone(
+          authController.countryDialCode + _emailController.text.trim(),
+          withCountryCode: true);
+      await authController.login(
+          phone != "" ? phone : _emailController.text.trim(),
+          _passwordController.text.trim(),
+          phone != "" ? "phone" : "email");
     }
   }
-
-
 }
-
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mohit/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mohit/Downloads/demandium-provider-app/Provider app"
+export "FLUTTER_APPLICATION_PATH=/Users/mohit/flutter_projects/freelance/Provider_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
